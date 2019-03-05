70bb24ce30801c4293b0e4be5789f22d61f7efb4
Alex Ollingworth <panicfugur@gmail.com>
2019-03-05 21:02:08 +0300

    More playing around

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import telegram
 from telegram.ext import Updater, CommandHandler
 import logging
+import random
+from responces import rabotaetliresplist
+
+def rabotaetli(bot, update):
+    number = random.randrange(0,2)
+    bot.send_message(chat_id=update.message.chat_id, text=rabotaetliresplist[number])
 
 
-def shutka(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Ну и шутка, пес!")
 
 
 updater = Updater(token="431555955:AAHmmmcJM2wcbJBlv7MbpaGVrAAVPgua6Pg")
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
-shutka_handler = CommandHandler('shutka', shutka)
-dispatcher.add_handler(shutka_handler)
+rabotaetli_handler = CommandHandler('rabotaetli', rabotaetli)
+dispatcher.add_handler(rabotaetli_handler)
 updater.start_polling()
