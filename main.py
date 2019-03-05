62894c7a5f9f7c58307bb8c33fc706b93b8a43bc
Alex Ollingworth <panicfugur@gmail.com>
2019-03-05 21:51:58 +0300

    Added proper shutka

--- main.py
+++ main.py
@@ -2,19 +2,34 @@ import telegram
 from telegram.ext import Updater, CommandHandler
 import logging
 import random
-from responces import rabotaetliresplist
+from responces import rabotaetliresplist, version, petrostickers
+
 
 def rabotaetli(bot, update):
-    number = random.randrange(0,2)
+    number = random.randrange(0,3)
     bot.send_message(chat_id=update.message.chat_id, text=rabotaetliresplist[number])
 
 
+def ver(bot, update):
+   bot.send_message(chat_id=update.message.chat_id, text=version) 
 
+
+def shutka(bot, update):
+    stickers = petrostickers
+    for x in range(5):
+        random.shuffle(stickers)
+        number = random.randrange(0, len(stickers)-1)
+        bot.send_sticker(chat_id=update.message.chat_id, sticker=stickers[number])
+    
 
 updater = Updater(token="431555955:AAHmmmcJM2wcbJBlv7MbpaGVrAAVPgua6Pg")
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 rabotaetli_handler = CommandHandler('rabotaetli', rabotaetli)
+ver_handler = CommandHandler('ver', ver)
+shutka_handler = CommandHandler('shutka', shutka)
 dispatcher.add_handler(rabotaetli_handler)
+dispatcher.add_handler(ver_handler)
+dispatcher.add_handler(shutka_handler)
 updater.start_polling()
--- responces.py
+++ responces.py
@@ -1,3 +1,21 @@
 rabotaetliresplist = ["Нет, не работает.",
                       "А почемы вы спрашиваете, молодой человек?",
                       "Нет, но я точно знаю что вы пес"]
+
+
+version = "PetrosyanBotPy версии 0.0.1"
+
+
+petrostickers = ['CAADAgADaQEAAklHHQABBEGA16tOb4YC',
+                 'CAADAgADawEAAklHHQABeErgzhVrOsMC',
+                 'CAADAgADbQEAAklHHQABRcfQMihFD5UC',
+                 'CAADAgADbwEAAklHHQABF5IwC-CbIWUC',
+                 'CAADAgADcQEAAklHHQABg-TAFSRKlBEC',
+                 'CAADAgADcwEAAklHHQABpTTP_kmMgMYC',
+                 'CAADAgADdQEAAklHHQABByV2nDznb64C',
+                 'CAADAgADdwEAAklHHQABv2sq89imy44C',
+                 'CAADAgADeQEAAklHHQABle7NQ8Mn9cQC',
+                 'CAADAgADewEAAklHHQABt152J5TO-1IC',
+                 'CAADAgADfQEAAklHHQABmDW9Zi5dogQC',
+                 'CAADAgADfwEAAklHHQAB_XyGP444HHMC',
+                 'CAADAgADCwEAAvR7GQABuArOzKHFjusC']
