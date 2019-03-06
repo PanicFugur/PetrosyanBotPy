b9246ae286495483a4062eaae1775fcbcdac5878
Alex Ollingworth <panicfugur@gmail.com>
2019-03-07 01:06:53 +0300

    Bringing code up to date

--- misc.py
+++ misc.py
@@ -13,7 +13,18 @@ class shuffle_bag:
 
     def __init__(self, items):
         self.items = items
+        self.source = items
 
+    def refill(func):
+        def wrapper(self):
+            if len(self.items) == 0:
+                self.items = self.source
+                func(self)
+            else:
+                func(self)
+        return wrapper
+
+    @refill
     def pop(self):
         random.shuffle(self.items)
         picked_item = self.items[len(self.items)-1]
