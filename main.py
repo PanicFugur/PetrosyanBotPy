df128ab984097707e573b8b266aaa71129fb84ac
Bombardir <panicfugur@gmail.com>
2019-03-07 20:46:03 +0300

    Shuffle_bag in use

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from responces import rabotaetliresplist, version, petrostickers, responceslist,
 from misc import load_file, shuffle_bag
 
 rabotaetlist = shuffle_bag(rabotaetliresplist)
+stickers = shuffle_bag(petrostickers)
+responces = shuffle_bag(responceslist)
+fish = shuffle_bag(fishstick)
 
 def rabotaetli(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=rabotaetlist.pop())
@@ -16,30 +19,22 @@ def ver(bot, update):
 
 
 def shutka(bot, update):
-    stickers = petrostickers
-    responces = responceslist
-    random.shuffle(responces)
-    bot.send_message(chat_id=update.message.chat_id, text=responces[0])
+    bot.send_message(chat_id=update.message.chat_id, text=responces.pop())
     for x in range(5):
-        random.shuffle(stickers)
-        number = random.randrange(0, len(stickers)-1)
         bot.send_sticker(chat_id=update.message.chat_id,
-                         sticker=stickers[number])
+                         sticker=stickers.pop())
 
 
-def fish(bot, update):
-    fish = fishstick
+def neznayu(bot, update):
     for x in range(5):
-        random.shuffle(fish)
-        number = random.randrange(0, len(fish)-1)
         bot.send_sticker(chat_id=update.message.chat_id,
-                         sticker=fish[number])
+                         sticker=fish.pop())
 
 
 def nebo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='А где неба нет?')
     bot.send_sticker(chat_id=update.message.chat_id,
-                     sticker=petrostickers[len(petrostickers)-1])
+                     sticker=petrostickers[-1])
 
 
 def topshutka(bot, update):
@@ -58,7 +53,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 rabotaetli_handler = CommandHandler('rabotaetli', rabotaetli) #Make loading smarter, like a list or something, it's an eyesore
 ver_handler = CommandHandler('ver', ver)
 shutka_handler = CommandHandler('shutka', shutka)
-fish_handler = CommandHandler('fish', fish)
+fish_handler = CommandHandler('fish', neznayu)
 nebo_handler = CommandHandler('nebo', nebo)
 topshutka_handler = CommandHandler('topshutka', topshutka)
 dispatcher.add_handler(rabotaetli_handler)
