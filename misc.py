4160fc98ff9b8e0d00225cff5c15396d070c911d
Bombardir <panicfugur@gmail.com>
2019-03-07 20:22:22 +0300

    More fixes for shufflebag

--- misc.py
+++ misc.py
@@ -14,19 +14,19 @@ class shuffle_bag:
 
     def __init__(self, items):
         self.items = items.copy()
-        self.source = items.copy()
+        self.source = self.items.copy()
 
     def draw(self):
         random.shuffle(self.items)
-        picked_item = self.items[len(self.items)-1]
-        self.items.pop(len(self.items)-1)
+        picked_item = self.items[-1]
+        self.items.pop(-1)
         return picked_item
 
     def pop(self):
-        if len(self.items) == 1:
-            self.items = self.source
+        if self.items:
             result = self.draw()
         else:
+            self.items = self.source.copy()
             result = self.draw()
         return result
 
