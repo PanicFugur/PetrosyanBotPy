07aa0a764df9762b86dbaa60d1062cb379547414
Bombardir <panicfugur@gmail.com>
2019-03-17 15:21:09 +0300

    Fix

--- qotdpicker.py
+++ qotdpicker.py
@@ -7,9 +7,10 @@ import logging
 
 from misc import shuffle_bag
 from responces import quotes
+from os.path import join
 
 try:
-    logging.basicConfig(filename='misc/app.log', format='%(asctime)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(filename=join(os.path.dirname(os.path.realpath(__file__)),'app.log'), format='%(asctime)s - %(message)s', level=logging.INFO)
     qoutelist = shuffle_bag(quotes, 'qotd')
     qotd = qoutelist.pop()
     with open('qotd', 'wb') as f:
