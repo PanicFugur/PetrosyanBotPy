68aa1caf7ef5d5e9a70607498c42495cc3a94151
Alex Ollingworth <panicfugur@gmail.com>
2019-10-01 20:01:42 +0300

    Added shuffle_bag v2

--- misc.py
+++ misc.py
@@ -3,6 +3,7 @@ import os
 import _pickle as pickle
 import random
 import time
+import json
 from os import listdir
 from os.path import isfile, join
 
@@ -55,3 +56,65 @@ class meme_bag:
         return join('memes', self.memes.pop())
 
 
+class shuffle_bag2:
+    # def __init__(self, sourceFile):
+    #     #need to follow the ask forgiveness not permission principle
+    #     self.source = sourceFile
+    #     if not os.path.isdir('misc'):
+    #         os.mkdir('misc')
+    #     if not (isfile(os.path.join('misc', sourceFile))):
+    #         print('The file doesnt exist')
+    #     if os.path.isdir('misc') and os.path.isfile(os.path.join('misc', sourceFile)):
+    #         with open((join(os.path.dirname(os.path.realpath(__file__)), 'misc', self.source)),'r') as f:
+    #             self.items = json.load(f)
+
+    def __init__(self, sourceFile):
+        self.source = sourceFile
+        try:
+            with open((join(os.path.dirname(os.path.realpath(__file__)), 'misc', self.source)),'r') as f:
+                self.items = json.load(f)
+        except OSError as e:
+            print(e)
+        except Exception as e:
+            print(e)
+
+
+
+
+
+    def draw(self):
+        result = []
+        for item in self.items.keys():
+            if self.items[item] == True:
+                result.append(item)
+        random.shuffle(result)
+        return result[-1]
+
+
+    def refresh(self):
+        for item in self.items:
+            self.items[item] = True
+
+    def needRefresh(self):
+        iter = 0
+        for item in self.items:
+            if (self.items[item] == False):
+                iter += 1
+        if iter == len(self.items):
+            return True
+        else:
+            return False
+
+    def pop(self):
+        if self.needRefresh():
+            self.refresh()
+        item = self.draw()
+        self.items[item] = False
+        with open((join(os.path.dirname(os.path.realpath(__file__)), 'misc', self.source)),'w') as f:
+            json.dump(self.items, f, ensure_ascii=False, indent=4, separators=(',',': '))
+        return item
+
+
+if __name__ == "__main__":
+    a = shuffle_bag2('petrosticker.json')
+    print(a.pop())
